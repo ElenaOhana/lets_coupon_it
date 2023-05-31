9077e97df0dbc734dd19eb4d81bf54ecbd2f62ff
Petina Elena <petinael708@gmail.com>
2023-05-31 13:53:00 +0300

    Update Config.ts

--- src/Utils/Config.ts
+++ src/Utils/Config.ts
@@ -12,12 +12,12 @@ class Config{
 } */
 
 class ProductionConfig extends Config{
-    public adminUrl: string = "https://lets-coupon-it.up.railway.app:7871/my-coupon-app/admin/";
-    public companyUrl: string = "https://lets-coupon-it.up.railway.app:7871/my-coupon-app/company/";
-    public customerUrl: string = "https://lets-coupon-it.up.railway.app:7871/my-coupon-app/customer/";
-    public couponsForAllUrl: string = "https://lets-coupon-it.up.railway.app:7871/my-coupon-app/customer/coupons-for-all"
-    public ableCouponsForAllUrl: string = "https://lets-coupon-it.up.railway.app:7871/my-coupon-app/customer/able-coupons-for-all"
-    public authUrl: string = "https://lets-coupon-it.up.railway.app:7871/my-coupon-app/auth/";
+    public adminUrl: string = "https://lets-coupon-it.up.railway.app/my-coupon-app/admin/";
+    public companyUrl: string = "https://lets-coupon-it.up.railway.app/my-coupon-app/company/";
+    public customerUrl: string = "https://lets-coupon-it.up.railway.app/my-coupon-app/customer/";
+    public couponsForAllUrl: string = "https://lets-coupon-it.up.railway.app/my-coupon-app/customer/coupons-for-all"
+    public ableCouponsForAllUrl: string = "https://lets-coupon-it.up.railway.app/my-coupon-app/customer/able-coupons-for-all"
+    public authUrl: string = "https://lets-coupon-it.up.railway.app/my-coupon-app/auth/";
 }
 
 /* const appConfig = process.env.NODE_ENV === "development" ? // If I do npm start => this is Development state, If I do nmp build => Production state.
